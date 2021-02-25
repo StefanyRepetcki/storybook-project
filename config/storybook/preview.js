224b232574f8f7a6f110ff18b41f0d8e75caa858
Stéfany Repetcki <stefany.c.r@hotmail.com>
2021-02-24 23:56:57 -0300

    feat: adding new configuration in storybook. refresh version storybook in package json, install bootstrap, ts config, prettier config, jest config, babel config, configuration theme, change theme

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,3 +1,16 @@
+import '../../src/assets/scss/main.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { configure } from '@storybook/vue';
+import BootstrapVue from 'bootstrap-vue';
+import Vue from 'vue';
+import bootstrap from 'bootstrap';
+
+Vue.use(BootstrapVue);
+
+configure(require.context('../../src/components', true, /\.stories\.js$/), module);
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: "^on[A-Z].*" },     
+  options: { storySort: { method: 'alphabetical'}, },
+}   
