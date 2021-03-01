b02993b71685ee829f934039a8175648542cfad5
Stéfany Repetcki <stefany.c.r@hotmail.com>
2021-03-01 00:46:47 -0300

    refactor: remove cod not used into accordion component

--- src/components/Accordion/Accordion.stories.ts
+++ src/components/Accordion/Accordion.stories.ts
@@ -4,7 +4,7 @@ export default {
 
 export const Accordion = () => ({
   template: `
-<div class="accordion accordion-flush">
+<div class="accordion accordion-flush border">
   <div class="accordion-item">
      <h2 class="accordion-header">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
@@ -13,26 +13,6 @@ export const Accordion = () => ({
      </h2>
      <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
-     </div>
-  </div>
-  <div class="accordion-item">
-     <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-        Accordion Item #2
-        </button>
-     </h2>
-     <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
-     </div>
-  </div>
-  <div class="accordion-item">
-     <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-        Accordion Item #3
-        </button>
-     </h2>
-     <div class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
      </div>
   </div>
 </div>`,
